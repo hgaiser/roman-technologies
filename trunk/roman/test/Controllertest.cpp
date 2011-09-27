5c08aa597aaf9ade7c233047e4519663e743c563
wilson_ko@live.com <wilson_ko@live.com@efe3342a-cd92-a411-41d2-ccb54d0c40cb>
2011-09-27 09:39:00 +0000

    rostest up and working

--- trunk/roman/test/Controllertest.cpp
+++ trunk/roman/test/Controllertest.cpp
@@ -1,25 +1,20 @@
-#include "roman/Controllertest.h"
+#include "roman/Controller.h"
 
 ros::Publisher keyTest_pub;
-ros::Publisher test_pub;
 ros::Subscriber sensorTest_sub;
 bool called = false;
 
 void activateSensorCBTest(const std_msgs::Empty::ConstPtr& msg){
     called = true;
-
-    printf("WORKS \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n ");
-    std::cout<<called<<std::endl;
 }
 
 ///Test whether the sensor will be activated when triangle key is toggled
 TEST(SensorCallBackTest, SensorActivated)
 {
-    std_msgs::Empty msg;
-    //roman::Key msg;
+    roman::Key msg;
     //Triangle button is pressed (button no. 12)
-    //msg.keys.push_back(12);
-    //msg.keys.push_back(255);
+    msg.keys.push_back(12);
+    msg.keys.push_back(255);
 
     test_pub.publish(msg);
     ros::Rate rate(10);
@@ -39,7 +34,6 @@ int main(int argc, char **argv){
     ros::init(argc, argv, "controllertest");
 
     ros::NodeHandle node;
-    test_pub        = node.advertise<std_msgs::Empty>("sensorTopic", 10);
     keyTest_pub     = node.advertise<roman::Key>("keyTopic", 10);
     sensorTest_sub  = node.subscribe("sensorTopic", 10, activateSensorCBTest);
 
