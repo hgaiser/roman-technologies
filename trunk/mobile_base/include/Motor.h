f0fc82fd6cf2b9cba657566906857584d2d5e56c
hansg91@gmail.com <hansg91@gmail.com@efe3342a-cd92-a411-41d2-ccb54d0c40cb>
2011-10-04 17:20:50 +0000

    Cleaned up code

--- trunk/mobile_base/include/Motor.h
+++ trunk/mobile_base/include/Motor.h
@@ -5,6 +5,10 @@
 #include <threemxl/C3mxlROS.h>
 #include <string>
 
+#define DEFAULT_P 0.01
+#define DEFAULT_I 0.0
+#define DEFAULT_D 0.0
+
 enum ControlMode
 {
     CM_NONE = -1,
@@ -19,18 +23,50 @@ enum ControlMode
     CM_TEST_MODE,       // for general testing
 };
 
+enum PIDParameter
+{
+	PID_PARAM_NONE = -1,
+	PID_PARAM_P,
+	PID_PARAM_I,
+	PID_PARAM_D,
+	PID_PARAM_MAX,
+};
+
+enum MotorId
+{
+	MID_NONE = -1,
+	MID_LEFT = 106,
+	MID_RIGHT = 107,
+	MID_ARM = 108,
+	MID_GRIPPER = 109,
+};
+
+
+class PID
+{
+public:
+	PID() : p(0.0), i(0.0), d(0.0) {}
+	PID(double proportional, double integral, double derivative) : p(proportional), i(integral), d(derivative) {}
+
+	double &operator[](unsigned int i);
+
+	double p;
+	double i;
+	double d;
+};
+
 /// Listens to motor commands and handles them accordingly.
 class Motor
 {
 protected:
-    int engine_number;      /// Motor id (left, right or arm motor)
+    MotorId mMotorId;      /// Motor id (left, right or arm motor)
     CDxlGeneric *motor_;    /// Motor interface
     LxSerial serial_port_;  /// Serial port interface
     ControlMode cmode;      /// Keeps track of what ControlMode the motor is in at the moment
 
 public:
     /// Constructor
-    Motor(int id) : engine_number(id), motor_(NULL), cmode(CM_NONE) { }
+    Motor(MotorId id) : mMotorId(id), motor_(NULL), cmode(CM_NONE) { }
     
     /// Destructor
     /** Delete motor interface, close serial port, and shut down node handle */
@@ -40,21 +76,22 @@ public:
             delete motor_;
         if (serial_port_.is_port_open())
             serial_port_.port_close();
-          
     }
 
     /// Initialize node
     /** \param path path to shared_serial node */
     void init(char *path);
 
-    void setMode(int mode);
+    void setMode(ControlMode mode);
     void setSpeed(double speed);
     void setAcceleration(double aceleration);
-    void setPID(double p, double i, double d);
+    void updatePID();
     void printPID();
 
     int getMode();
     int getID();
+
+    PID mPID;
 };    
 
 #endif /* __MOTOR_H */
--- trunk/mobile_base/include/MotorHandler.h
+++ trunk/mobile_base/include/MotorHandler.h
@@ -9,22 +9,28 @@
 #include <sensor_msgs/Joy.h>
 #include <mobile_base/tweak.h>
 #include <threemxl/dxlassert.h>
+#include <BaseController.h>
+
+#define wheel_radius    0.1475
 
 /// Listens to motor commands and handles them accordingly.
 class MotorHandler
 {
 protected:
-    ros::NodeHandle nh_;             /// ROS node handle
-    ros::Subscriber twist_sub;       /// Listens to Twist messages for movement
-    ros::Subscriber tweak_sub;       /// Listens to Int messages, the integers represent the pressed DPAD button on the PS3 controller
+    ros::NodeHandle nh_;			/// ROS node handle
+    ros::Subscriber twist_sub;		/// Listens to Twist messages for movement
+    ros::Subscriber tweak_sub;		/// Listens to Int messages, the integers represent the pressed DPAD button on the PS3 controller
 
-    Motor left_engine;               /// Motor for left wheel
-    Motor right_engine;              /// Motor for right wheel
-    //Motor arm_engine;              /// Motor for arm
+    Motor left_engine;				/// Motor for left wheel
+    Motor right_engine;				/// Motor for right wheel
+    //Motor arm_engine;				/// Motor for arm
+
+    MotorId mMotorId;				/// Id of the motor that is currently being controlled
+    PIDParameter mPIDFocus;			/// One of the PID parameters that is to be changed on button events
 
 public:
     /// Constructor
-    MotorHandler() : nh_("~"), left_engine(106), right_engine(107) { }
+    MotorHandler() : nh_("~"), left_engine(MID_LEFT), right_engine(MID_RIGHT), mMotorId(MID_LEFT), mPIDFocus(PID_PARAM_I) { }
     
     /// Destructor
     /** Delete motor interface, close serial port, and shut down node handle */
