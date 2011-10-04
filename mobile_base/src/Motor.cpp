e5f83818a337abc9778809d9f31d191faf3c0ac5
hansg91@gmail.com <hansg91@gmail.com@efe3342a-cd92-a411-41d2-ccb54d0c40cb>
2011-10-04 17:20:50 +0000

    Cleaned up code

--- mobile_base/src/Motor.cpp
+++ mobile_base/src/Motor.cpp
@@ -2,69 +2,55 @@
 
 using namespace std;
 
+double &PID::operator[](unsigned int i)
+{
+	switch (i)
+	{
+	case PID_PARAM_P: return this->p;
+	case PID_PARAM_I: return this->i;
+	case PID_PARAM_D: return this->d;
+	default: break;
+	}
+
+	assert(false); // something went terribad
+}
+
 /**
   * Sets the mode of the motor
  */
-void Motor::setMode(int mode){
-    switch(mode){
-    case SPEED_MODE:
-        cmode = CM_SPEED_MODE;
-        motor_->set3MxlMode(SPEED_MODE);
-        cout << "Changed to speed mode" << endl;
-        break;
+void Motor::setMode(ControlMode mode)
+{
+	if (cmode != mode)
+		return; // already in this mode, nothing needs to be done
 
-    case TORQUE_MODE:
-        cmode = CM_TORQUE_MODE;
-        motor_->set3MxlMode(TORQUE_MODE);
-        cout << "Changed to torque mode" << endl;
-        break;
-
-    case CURRENT_MODE:
-        cmode = CM_CURRENT_MODE;
-        motor_->set3MxlMode(CURRENT_MODE);
-        cout << "Changed to current mode" << endl;
-        break;
-
-    case STOP_MODE:
-        cmode = CM_STOP_MODE;
-        cout << "Changed to stop mode" << endl;
-        motor_->set3MxlMode(STOP_MODE);
-        break;
-
-    case POSITION_MODE:
-        cmode = CM_POSITION_MODE;
-        cout << "Changed to position mode" << endl;
-        motor_->set3MxlMode(POSITION_MODE);
-        break;
-
-    case SEA_MODE:
-        cmode = CM_SEA_MODE;
-        cout << "Changed to sea mode" << endl;
-        motor_->set3MxlMode(SEA_MODE);
-        break;
-
-    case PWM_MODE:
-        cmode = CM_PWM_MODE;
-        cout << "Changed to pwm mode" << endl;
-        motor_->set3MxlMode(PWM_MODE);
-        break;
-
-    case TEST_MODE:
-        cmode = CM_TEST_MODE;
-        cout << "Changed to test mode" << endl;
-        motor_->set3MxlMode(TEST_MODE);
-        break;
+	cmode = mode;
+	motor_->set3MxlMode(mode);
+	cout << "Changed to ";
+    switch (mode)
+    {
+    case SPEED_MODE: cout << "speed"; break;
+    case TORQUE_MODE: cout << "torque"; break;
+    case CURRENT_MODE: cout << "current"; break;
+    case STOP_MODE: cout << "stop"; break;
+    case POSITION_MODE: cout << "position"; break;
+    case SEA_MODE: cout << "sea"; break;
+    case PWM_MODE: cout << "pwm"; break;
+    case TEST_MODE: cout << "test"; break;
+    default : break;
     }
+    cout << " mode." << endl;
 }
 
 
 /**
   * Sets acceleration of the motor when in SPEED_MODE
  */
-void Motor::setAcceleration(double acceleration){
-    if(cmode != SPEED_MODE){
+void Motor::setAcceleration(double acceleration)
+{
+    if (cmode != CM_SPEED_MODE)
+    {
         cout << "Motor not in speed mode" << endl;
-        setMode(SPEED_MODE);
+        setMode(CM_SPEED_MODE);
     }
 
     motor_->setAcceleration(acceleration);
@@ -73,81 +59,46 @@ void Motor::setAcceleration(double acceleration){
 /**
   * Sets speed of the motor when in SPEED_MODE
  */
-void Motor::setSpeed(double speed){
-        setAcceleration(5);
-        motor_->setSpeed(speed);        
+void Motor::setSpeed(double speed)
+{
+	setAcceleration(5);
+    motor_->setSpeed(speed);
 }
 
 /**
  * Sets speed pid
  */
-void Motor::setPID(double p, double i, double d){
-    motor_->setPIDSpeed(p, d, i, 0, false);
+void Motor::updatePID()
+{
+    motor_->setPIDSpeed(mPID.p, mPID.d, mPID.i, 0, false);
 }
 
 /**
  * Gets speed pid
  */
-void Motor::printPID(){
-	double p;
-	double i;
-	double d;
-	double i_limit;
-
+void Motor::printPID()
+{
+	double p, i, d, i_limit;
 	motor_->getPIDSpeed(p, d, i, i_limit);
 
 	ROS_INFO("p: %f, i: %f, d: %f, i_limit: %f \n", p, i, d, i_limit);
 }
 
-int Motor::getMode(){
-    switch(cmode){
-    case CM_NONE:
-        return -1;
-
-    case CM_SPEED_MODE:
-        return SPEED_MODE;
-
-    case CM_TORQUE_MODE:
-        return TORQUE_MODE;
-
-    case CM_CURRENT_MODE:
-        return CURRENT_MODE;
-
-    case CM_STOP_MODE:
-        return STOP_MODE;
-
-    case CM_POSITION_MODE:
-        return POSITION_MODE;
-
-    case CM_SEA_MODE:
-        return SEA_MODE;
-
-    case CM_PWM_MODE:
-        return PWM_MODE;
-
-    case CM_TEST_MODE:
-        return TEST_MODE;
-    }
-
-    return -2;
-}
-
-int Motor::getID(){
-    return engine_number;
-}
-
 /**
  * Initalize Motor and its attributes.
 */
-void Motor::init(char *path){
+void Motor::init(char *path)
+{
     CDxlConfig *config = new CDxlConfig();
 
     // find the connection to the motor on the 3mXl board
-    if (path){
+    if (path)
+    {
         cout << "Using shared_serial"<< endl;
         motor_ = new C3mxlROS(path);
     }
-    else{
+    else
+    {
         cout << "Using direct connection" << endl;
         motor_ = new C3mxl();
 
@@ -158,11 +109,10 @@ void Motor::init(char *path){
     }
 
     // initialize the motor
-    motor_->setConfig(config->setID(engine_number));
+    motor_->setConfig(config->setID(mMotorId));
     motor_->init(false);
-    setMode(STOP_MODE);
+    setMode(CM_STOP_MODE);
 
     delete config;
-    cout << "Motor " << engine_number << " initializing completed." << endl;
+    cout << "Motor " << mMotorId << " initializing completed." << endl;
 }
-
--- mobile_base/src/MotorHandler.cpp
+++ mobile_base/src/MotorHandler.cpp
@@ -1,14 +1,5 @@
 #include <MotorHandler.h>
 
-#define wheel_radius    0.1475
-#define LEFT_MOTOR		106
-#define RIGHT_MOTOR		107
-#define UP              4
-#define RIGHT           5
-#define DOWN            6
-#define L1				10
-#define R1				11
-
 /**
  * Called when a Twist message is received over the motor topic.
  */
@@ -21,100 +12,42 @@ void MotorHandler::moveCB(const geometry_msgs::Twist& msg)
 	left_engine.setSpeed(left_vel);
 }
 
-double p_left, p_right = 0.01;
-double i_left, i_right = 0;
-double d_left, d_right = 0;
-int ID;
-int toSet = 0;
-
 /**
  * Called when a Twist message is received over the motor topic.
 */
 void MotorHandler::tweakCB(const mobile_base::tweak msg)
 {
-	double *p = ID == LEFT_MOTOR ? &p_left : &p_right;
-	double *i = ID == LEFT_MOTOR ? &i_left : &i_right;
-	double *d = ID == LEFT_MOTOR ? &d_left : &d_right;
+	Motor *motor = mMotorId == MID_LEFT ? &left_engine : &right_engine;
 
-	if(msg.data == UP)
+	switch (msg.data)
 	{
-		switch(toSet){
-		case 0:
-			*p += 0.01;
-			break;
+	case PS3_UP:
+	case PS3_DOWN:
+		motor->mPID[mPIDFocus] += msg.data == PS3_DOWN ? -0.01 : 0.01;
+		motor->mPID[mPIDFocus] = std::max(0.0, motor->mPID[mPIDFocus]);
+		motor->updatePID();
+		motor->printPID();
+		break;
 
-		case 1:
-			*i += 0.01;
-			break;
+	case PS3_RIGHT:
+		// toggle through P-I-D
+		mPIDFocus = PIDParameter((mPIDFocus + 1) % PID_PARAM_MAX);
 
-		case 2:
-			*d += 0.01;
-			break;
-		}
-		if(ID == LEFT_MOTOR)
+		switch (mPIDFocus)
 		{
-			left_engine.setPID(*p, *i, *d);
-			left_engine.printPID();
+		case PID_PARAM_P: ROS_INFO("P"); break;
+		case PID_PARAM_I: ROS_INFO("I"); break;
+		case PID_PARAM_D: ROS_INFO("D"); break;
+		default: break;
 		}
-			else
-			{
-				right_engine.setPID(*p, *i, *d);
-				right_engine.printPID();
-			}
-	}
 
-	if(msg.data == DOWN)
-	{
-		switch(toSet){
-		case 0:
-			if(*p > 0)
-				*p -= 0.01;
-			break;
+		break;
 
-		case 1:
-			if(*i > 0)
-				*i -= 0.01;
-			break;
-
-		case 2:
-			if(*d > 0)
-				*d -= 0.01;
-			break;
-		}
-		if(ID == LEFT_MOTOR)
-		{
-			left_engine.setPID(*p, *i, *d);
-			left_engine.printPID();
-		}
-		else
-		{
-			right_engine.setPID(*p, *i, *d);
-			right_engine.printPID();
-		}
-	}
-
-	if(msg.data == RIGHT){
-		toSet = (toSet + 1)%3;
-
-		if(toSet == 0)
-			ROS_INFO("P");
-
-		if(toSet == 1)
-			ROS_INFO("I");
-
-		if(toSet == 2)
-			ROS_INFO("D");
-	}
-
-	if(msg.data == L1 || msg.data == R1){
-		if(msg.motorID == LEFT_MOTOR){
-			ROS_INFO("SWITCHED TO LEFT ENGINE");
-			ID = LEFT_MOTOR;
-		}
-		else{
-			ROS_INFO("SWITCHED TO RIGHT ENGINE");
-			ID = RIGHT_MOTOR;
-		}
+	case PS3_L1:
+	case PS3_R1:
+		mMotorId = MotorId(msg.motorID);
+		ROS_INFO("SWITCHED TO %s ENGINE", mMotorId == MID_LEFT ? "LEFT" : "RIGHT");
+		break;
 	}
 }
 
