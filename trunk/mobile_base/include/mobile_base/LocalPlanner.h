8852e060e6691e60d4c8fea15357e42dd8704936
hansg91@gmail.com <hansg91@gmail.com@efe3342a-cd92-a411-41d2-ccb54d0c40cb>
2011-12-13 16:16:52 +0000

    minor fixup

--- trunk/mobile_base/include/mobile_base/LocalPlanner.h
+++ trunk/mobile_base/include/mobile_base/LocalPlanner.h
@@ -8,17 +8,12 @@
 #ifndef LOCALPLANNER_H_
 #define LOCALPLANNER_H_
 
-#include "ros/ros.h"
-
 class LocalPlanner: public BaseLocalPlanner
 {
-private:
-	ros::NodeHandle mNodeHandle;
-
 public:
     virtual bool computeVelocityCommands(geometry_msgs::Twist& cmd_vel) { return false; };
     virtual bool isGoalReached() { return true; };
-    virtual bool setPlan(const std::vector<geometry_msgs::PoseStamped>& plan);
+    virtual bool setPlan(const std::vector<geometry_msgs::PoseStamped>& plan) { return false; };
     virtual void initialize(std::string name, tf::TransformListener* tf, costmap_2d::Costmap2DROS* costmap_ros);
 };
 
