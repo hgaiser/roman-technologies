d9359f21493d1761f1c696b267a2f41e1bf2d461
wilson_ko@live.com <wilson_ko@live.com@efe3342a-cd92-a411-41d2-ccb54d0c40cb>
2011-12-22 08:56:08 +0000

    removed input package

--- trunk/arm/include/AutonomeArmController.h
+++ trunk/arm/include/AutonomeArmController.h
@@ -5,15 +5,27 @@
 #define EXIT_SUCCESS 0
 #endif
 
-
 #include <ros/ros.h>
+#include "arm/armJointPos.h"
+#include "arm/armCoordinatesPos.h"
+#include "arm/IK.h"
 
+#define MIN_Z_VALUE -27.5
+#define MAX_Z_VALUE 18.0
+#define MIN_X_VALUE 0
+#define MAX_X_VALUE 33.4
 
 
 class AutonomeArmController
 {
 protected:
-	ros::NodeHandle mNodeHandle;
+	ros::NodeHandle mNodeHandle;				//ROS node handler
+	ros::Publisher mJointCommandPublisher;		//Publishes commands for ArmMotorHandler
+	ros::Subscriber mCurrentPositionSubscriber;	//Listens to current positions in coordinates of configuration space
+	ros::Subscriber mCommandSubscriber;			//Listens to commands in coordinates
+	ros::ServiceClient mKinematicsClient;		//Client to call for service to calculate inverse kinematics
+
+	arm::armCoordinatesPos mCurrentPos;			//Keeps track of current position in coordinates of configuration space
 
 public:
 	AutonomeArmController(): mNodeHandle(""){}
@@ -23,6 +35,8 @@ public:
 	}
 
 	void init();
+	void armPositionCB(const arm::armCoordinatesPos &msg);
+	void cmdCB(const arm::armCoordinatesPos &msg);
 };
 
 
--- trunk/arm/src/AutonomeArmController.cpp
+++ trunk/arm/src/AutonomeArmController.cpp
@@ -1,30 +1,70 @@
 #include "AutonomeArmController.h"
 
-
-
-
-
-
-void AutonomeArmController::init()
+/*
+ *	Listens to commands and translates it to joint commands for ArmMotorHandler using inverse kinematics
+ */
+void AutonomeArmController::cmdCB(const arm::armCoordinatesPos &msg)
 {
-	ROS_INFO("AutonomeArmController initialized");
+	arm::IK srv;
+
+	//Listen to command
+	srv.request.target = msg;
+
+	//Check constraints
+	if(msg.z_value > MAX_Z_VALUE)
+		srv.request.target.z_value = MAX_Z_VALUE;
+	else if(msg.z_value < MIN_Z_VALUE)
+		srv.request.target.z_value = MIN_Z_VALUE;
+
+	if(msg.x_value > MAX_X_VALUE)
+		srv.request.target.x_value = MAX_X_VALUE;
+	else if(msg.x_value < MIN_Z_VALUE)
+		srv.request.target.z_value = MIN_X_VALUE;
+
+	//Calculate joint configurations with IK solver
+	if((msg.z_value < MAX_Z_VALUE) && (msg.z_value > MIN_Z_VALUE) && (msg.x_value < MAX_X_VALUE) && (msg.x_value < MAX_X_VALUE))
+		mKinematicsClient.call(srv);
+
+	//Call trajectory planner service?
+
+	arm::armJointPos cmd_msg;
+
+	//Send command to Motorhandler
+	cmd_msg.upper_joint = srv.response.configuration.upper_joint;
+	cmd_msg.wrist_joint = srv.response.configuration.wrist_joint;
+
+	mJointCommandPublisher.publish(cmd_msg);
+}
+//TODO make x dependent on z as constraint
+
+/*
+ * Keep track of current position in configuration space
+ */
+void AutonomeArmController::armPositionCB(const arm::armCoordinatesPos &msg)
+{
+	mCurrentPos = msg;
 }
 
-
-
-
+/*
+ * Initialize AutonomeArmController node
+ */
+void AutonomeArmController::init()
+{
+	mJointCommandPublisher			= mNodeHandle.advertise<std_msgs::Float64>("/ShoulderTopic", 10);	//temporary float64
+	mCurrentPositionSubscriber 		= mNodeHandle.subscribe("/armCoordinatePositionFeedbackTopic", 1, &AutonomeArmController::armPositionCB, this);
+	mCommandSubscriber 				= mNodeHandle.subscribe("/cmd_position", 1, &AutonomeArmController::cmdCB, this);
+	mKinematicsClient 				= mNodeHandle.serviceClient<arm::IK>("IK");
+	ROS_INFO("AutonomeArmController initialized");
+}
 
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "AutonomeArmController");
 
-	AutonomeArmController* ArmController = new AutonomeArmController();
-	ArmController->init();
+	AutonomeArmController armController;
+	armController.init();
 
-	while(ros::ok())
-	{
-		ros::spin();
-	}
+	ros::spin();
 
 	return EXIT_SUCCESS;	// 0
 }
