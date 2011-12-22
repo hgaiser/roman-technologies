942f2c2f92f8e86216d8134d8c11c3f823062337
hansg91@gmail.com <hansg91@gmail.com@efe3342a-cd92-a411-41d2-ccb54d0c40cb>
2011-12-22 09:16:08 +0000

    The PathFollower should now stop before turning/moving and should take the final yaw into account

--- logical_unit/src/PathFollower.cpp
+++ logical_unit/src/PathFollower.cpp
@@ -7,16 +7,20 @@
 
 #include "logical_unit/PathFollower.h"
 
-PathFollower::PathFollower() : mPathIndex(0)
+PathFollower::PathFollower() : mPathIndex(0), mFollowState(FOLLOW_STATE_IDLE), mAllowState(FOLLOW_STATE_IDLE)
 {
-	std::string pathTopic;
+	std::string pathTopic, speedFeedbackTopic;
 	mNodeHandle.param<std::string>("path_topic", pathTopic, "/global_path");
+	mNodeHandle.param<std::string>("speed_feedback_topic", pathTopic, "/speedFeedbackTopic");
 	mNodeHandle.param<int>("refresh_rate", mRefreshRate, 5);
 	mNodeHandle.param<double>("yaw_tolerance", mYawTolerance, 0.2);
 	mNodeHandle.param<double>("angular_speed", mAngularSpeed, 0.2);
 	mNodeHandle.param<double>("linear_speed", mLinearSpeed, 0.2);
+	mNodeHandle.param<double>("disable_transition_threshold", mDisableTransitionThreshold, 0.05);
+	mNodeHandle.param<double>("final_yaw_tolerance", mFinalYawTolerance, 0.1);
 
 	mPathSub = mNodeHandle.subscribe(pathTopic, 1, &PathFollower::pathCb, this);
+	mSpeedFeedbackSub = mNodeHandle.subscribe(speedFeedbackTopic, 1, &PathFollower::pathCb, this);
 	mCommandPub = mNodeHandle.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
 }
 
@@ -49,47 +53,57 @@ void PathFollower::handlePath(tf::TransformListener *transformListener)
 			mRobotPosition.getOrigin().getY(), mRobotPosition.getOrigin().getZ(), mPath.poses[mPathIndex+1].pose.position.x,
 			mPath.poses[mPathIndex+1].pose.position.y, mPath.poses[mPathIndex+1].pose.position.z);
 
+	double robotYaw = tf::getYaw(mRobotPosition.getRotation());
+	double targetYaw = tf::getYaw(mPath.poses[mPathIndex+1].pose.orientation);
+
 	btVector3 robotPos(mPath.poses[mPathIndex+1].pose.position.x, mPath.poses[mPathIndex+1].pose.position.y, mPath.poses[mPathIndex+1].pose.position.z);
 	double dist = mRobotPosition.getOrigin().distance(robotPos);
-	if (dist < 0.2)
+	if (dist < 0.2 && (mPathIndex + 1 != int(mPath.poses.size() - 1) || fabs(robotYaw - targetYaw) < mFinalYawTolerance))
 	{
 		ROS_INFO("Reached waypoint.");
 		continuePath();
 		return;
 	}
 
-	double yaw = tf::getYaw(mRobotPosition.getRotation());
-
-	double dx = mPath.poses[mPathIndex+1].pose.position.x - mRobotPosition.getOrigin().getX();
-	double dy = mPath.poses[mPathIndex+1].pose.position.y - mRobotPosition.getOrigin().getY();
-	if (dx == 0.0)
-		dx = 0.01;
-	double requiredYaw = atan(fabs(dy / dx));
-	if (dx < 0.0)
+	double requiredYaw;
+	if (mPathIndex + 1 == int(mPath.poses.size() - 1))
 	{
-		if (dy < 0.0)
-			requiredYaw = -M_PI + requiredYaw;
-		else
-			requiredYaw = M_PI - requiredYaw;
+		double dx = mPath.poses[mPathIndex+1].pose.position.x - mRobotPosition.getOrigin().getX();
+		double dy = mPath.poses[mPathIndex+1].pose.position.y - mRobotPosition.getOrigin().getY();
+		if (dx == 0.0)
+			dx = 0.01;
+		requiredYaw = atan(fabs(dy / dx));
+		if (dx < 0.0)
+		{
+			if (dy < 0.0)
+				requiredYaw = -M_PI + requiredYaw;
+			else
+				requiredYaw = M_PI - requiredYaw;
+		}
+		else if (dy < 0.0)
+			requiredYaw = -1 * requiredYaw;
 	}
-	else if (dy < 0.0)
-		requiredYaw = -1 * requiredYaw;
-	double diff = requiredYaw - yaw;
+	else
+	{
+		requiredYaw = targetYaw;
+		ROS_INFO("Turning for final orientation.");
+	}
+	double diff = requiredYaw - robotYaw;
 
 	// do we need to go rotate?
-	if (fabs(diff) > mYawTolerance)
+	if ((mAllowState == FOLLOW_STATE_IDLE || mAllowState == FOLLOW_STATE_TURNING) &&
+			fabs(diff) > mYawTolerance)
 	{
-		ROS_INFO("Trying to rotate; yaw: %lf, requiredYaw: %lf, diff: %lf", yaw, requiredYaw, diff);
+		ROS_INFO("Trying to rotate; yaw: %lf, requiredYaw: %lf, diff: %lf", robotYaw, requiredYaw, diff);
 
+		mFollowState = FOLLOW_STATE_TURNING;
 		command.angular.z = mAngularSpeed;
 		if (diff < 0.0)
 			command.angular.z = -command.angular.z;
-
-		mCommandPub.publish(command);
-		return;
 	}
+	else if (mAllowState == FOLLOW_STATE_IDLE || mAllowState == FOLLOW_STATE_FORWARD)// we need to go forward
+		command.linear.x = mLinearSpeed;
 
-	command.linear.x = mLinearSpeed;
 	mCommandPub.publish(command);
 }
 
@@ -112,6 +126,18 @@ void PathFollower::pathCb(const nav_msgs::Path &path)
 	ROS_INFO("Received new path.");
 	mPath = path;
 	mPathIndex = 0;
+}
+
+void PathFollower::speedCb(const geometry_msgs::Twist &twist)
+{
+	double vel_left = twist.linear.x + twist.angular.z / 2;
+	double vel_right = twist.linear.x - twist.angular.z / 2;
+
+	if ((fabs(vel_left) > mDisableTransitionThreshold || fabs(vel_right) > mDisableTransitionThreshold) &&
+			mFollowState != FOLLOW_STATE_IDLE)
+		mAllowState = mFollowState;
+	else
+		mAllowState = FOLLOW_STATE_IDLE;
 }
 
 int main(int argc, char **argv)
