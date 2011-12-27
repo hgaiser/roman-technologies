d706350a3de50397554d696eadf0ce69c252d5d5
wilson_ko@live.com <wilson_ko@live.com@efe3342a-cd92-a411-41d2-ccb54d0c40cb>
2011-12-27 14:43:24 +0000

    object recognition

---
+++ image_processing/src/ObjectRecognition.cpp
@@ -0,0 +1,114 @@
+/*
+ * ArmTrajectoryPlanner.cpp
+ *
+ *  Created on: 2011-11-13
+ *      Author: wilson
+ */
+
+#include "image_processing/ObjectRecognition.h"
+
+ros::ServiceClient mObjectRecognizeClient;	//Service client for tabletop_object_detection
+ros::ServiceClient mCollisionmapClient;		//Service client for tabletop_collision_map_processing
+
+void ObjectRecognition::recognizeCB(const std_msgs::Bool &msg)
+{
+
+}
+
+void ObjectRecognition::init()
+{
+	//wait for detection client
+	while ( !ros::service::waitForService("/object_detection",
+			ros::Duration(2.0)) && mNodeHandle.ok() )
+	{
+		ROS_INFO("Waiting for object detection service to come up");
+	}
+	if (!mNodeHandle.ok())
+		exit(0);
+
+	mObjectRecognizeClient = mNodeHandle.serviceClient<tabletop_object_detector::TabletopDetection>("/object_detection", true);
+
+	//wait for collision map processing client
+	while ( !ros::service::waitForService("/tabletop_collision_map_processing/tabletop_collision_map_processing",
+			ros::Duration(2.0)) && mNodeHandle.ok() )
+	{
+		ROS_INFO("Waiting for collision processing service to come up");
+	}
+	if (!mNodeHandle.ok())
+		exit(0);
+
+	mCollisionmapClient = mNodeHandle.serviceClient<tabletop_collision_map_processing::TabletopCollisionMapProcessing>("/tabletop_collision_map_processing/tabletop_collision_map_processing", true);
+
+	//initialise subscribers
+	mCommandSubscriber = mNodeHandle.subscribe("/objectRecognizeCommandTopic", 1, &ObjectRecognition::recognizeCB, this);
+
+	//initialise publishers
+}
+
+int main(int argc, char **argv)
+{
+	// init ros and ObjectRecognition
+	ros::init(argc, argv, "ObjectRecognition");
+	ObjectRecognition objectRecognition;
+
+	char *path = NULL;
+	if (argc == 2)
+		path = argv[1];
+
+	objectRecognition.init();
+
+	//call the tabletop detection
+	ROS_INFO("Calling tabletop detector");
+	tabletop_object_detector::TabletopDetection detection_call;
+	//we want recognized database objects returned
+	//set this to false if you are using the pipeline without the database
+	detection_call.request.return_clusters = true;
+	//we want the individual object point clouds returned as well
+	detection_call.request.return_models = true;
+	detection_call.request.num_models = 1;
+
+	if (!mObjectRecognizeClient.call(detection_call))
+	{
+		ROS_ERROR("Tabletop detection service failed");
+		return -1;
+	}
+	if (detection_call.response.detection.result !=
+			detection_call.response.detection.SUCCESS)
+	{
+		ROS_ERROR("Tabletop detection returned error code %d",
+				detection_call.response.detection.result);
+		return -1;
+	}
+	if (detection_call.response.detection.clusters.empty() &&
+			detection_call.response.detection.models.empty() )
+	{
+		ROS_ERROR("The tabletop detector detected the table, "
+				"but found no objects");
+		return -1;
+	}
+
+	//call collision map processing
+	ROS_INFO("Calling collision map processing");
+	tabletop_collision_map_processing::TabletopCollisionMapProcessing processing_call;
+	//pass the result of the tabletop detection
+	processing_call.request.detection_result = detection_call.response.detection;
+	//ask for the existing map and collision models to be reset
+	processing_call.request.reset_collision_models = true;
+	processing_call.request.reset_attached_models = true;
+	//ask for the results to be returned in base link frame
+	processing_call.request.desired_frame = "base_link";
+
+	if (!mCollisionmapClient.call(processing_call))
+	{
+		ROS_ERROR("Collision map processing service failed");
+		return -1;
+	}
+	//the collision map processor returns instances of graspable objects
+	if (processing_call.response.graspable_objects.empty())
+	{
+		ROS_ERROR("Collision map processing returned no graspable objects");
+		return -1;
+	}
+
+	ros::spin();
+}
