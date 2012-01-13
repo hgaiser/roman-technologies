bd157ba1f2f64f9c6db0602d7ebdf4dc904b3177
wilson_ko@live.com <wilson_ko@live.com@efe3342a-cd92-a411-41d2-ccb54d0c40cb>
2012-01-13 12:59:59 +0000

    Find object in ObjectRecognition (faulty)

--- image_processing/include/image_processing/ObjectRecognition.h
+++ image_processing/include/image_processing/ObjectRecognition.h
@@ -9,10 +9,11 @@
 #define OBJECTRECOGNITION_H_
 
 #include <ros/ros.h>
-#include <std_msgs/Bool.h>
+#include <std_msgs/UInt8.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/ros/conversions.h>
+#include <geometry_msgs/PoseStamped.h>
 #include <sensor_msgs/point_cloud_conversion.h>
 #include <tabletop_object_detector/TabletopDetection.h>
 #include <tabletop_collision_map_processing/TabletopCollisionMapProcessing.h>
@@ -20,10 +21,13 @@
 class ObjectRecognition
 {
 protected:
-	ros::NodeHandle mNodeHandle;				//ROS node handler
-	ros::Subscriber mCommandSubscriber;			//Listens to commands to start image recognition
 	tabletop_collision_map_processing::TabletopCollisionMapProcessing mCollisionmap;
 	tabletop_object_detector::TabletopDetection mObject;
+
+	ros::NodeHandle mNodeHandle;				//ROS node handler
+	ros::Subscriber mCommandSubscriber;			//Listens to commands to start image recognition
+	ros::Publisher 	mObjectPosePublisher;		//Publishes the pose of the recognised object
+	int mObjectToFind;							//ID of the object the robot should look for
 
 public:
 	ObjectRecognition(): mNodeHandle(""){}
@@ -34,7 +38,17 @@ public:
 
 	void init();
 
-	void recognizeCB(const std_msgs::Bool &msg);
+	void recognizeCB(const std_msgs::UInt8 &msg);
+
+	int inline getObjectID()
+	{
+		return mObjectToFind;
+	}
+
+	void inline publishObjectPose(const geometry_msgs::PoseStamped msg)
+	{
+		mObjectPosePublisher.publish(msg);
+	}
 };
 
 #endif /* OBJECTRECOGNITION_H_ */
