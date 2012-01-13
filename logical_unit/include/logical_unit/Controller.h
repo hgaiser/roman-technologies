5ababd41126ea3762f7746046f9d1c40b6df723e
wilson_ko@live.com <wilson_ko@live.com@efe3342a-cd92-a411-41d2-ccb54d0c40cb>
2012-01-13 12:59:09 +0000

    logical unit with Controller (not finished)

--- logical_unit/include/logical_unit/Controller.h
+++ logical_unit/include/logical_unit/Controller.h
@@ -9,8 +9,22 @@
 #define CONTROLLER_H_
 
 #include <ros/ros.h>
+#include <std_msgs/UInt8.h>
+#include <std_msgs/Float32.h>
+#include <logical_unit/PathFollower.h>
 #include <audio_processing/speech.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Pose.h>
+#include "yaml-cpp/yaml.h"
+#include "fstream"
 #include <map>
+
+#define MIN_ARM_Z_VALUE -32.1
+#define MAX_ARM_Z_VALUE 17.25
+#define MIN_ARM_X_VALUE -20
+#define MAX_ARM_X_VALUE 35.95
 
 enum commandValue
 {
@@ -32,17 +46,33 @@ class Controller
 {
 private:
 	ros::NodeHandle mNodeHandle;
-	//ros::Publisher mGripperPublisher;
-	//ros::Publisher mArmPublisher;
-	//ros::Publisher mHeadPublisher;
 
-	//ros::Subscriber mKinectSubscriber;
-	ros::Subscriber mSpeechSubscriber;		/// Listens to speech processed by SpeechRecognition node
+	ros::Subscriber mSpeechSubscriber;			/// Listens to speech processed by SpeechRecognition node
+	ros::Subscriber mObjectPoseSubscriber;		/// Listens to the Pose of the found object
+	ros::Subscriber mBaseGoalSubscriber;		/// Listens to path distances to see whether goal is reached
+	ros::Subscriber mNavigationStateSubscriber;	///	Listens to the state of the navigation algorithm
 
-	bool mWakeUp;							/// Keeps track of whether Nero is listening to commands or not
-	std::string mSpeech;					/// Keeps track of what is said by the user
-	uint8_t mArousal;						/// Keeps track of the arousal contained in the speech of user
-	Emotions mEmotionalState;				/// Keeps track of Nero's current emotion
+	ros::Publisher mBaseGoalPublisher;			/// Publishes goal commands to PathPlanner
+	ros::Publisher mArmPositionPublisher;		/// Publishes coordinates to AutonomeArmController
+	ros::Publisher mHeadPositionPublisher;		/// Publishes coordinates to AutonomeHeadController
+	ros::Publisher mObjectRecognitionPublisher;	/// Publishes ID of the object to recognize
+
+	tf::TransformListener mTransformListener;	/// Fills mOriginalPosition
+	tf::StampedTransform mOriginalPosition;		/// Keeps track of the original position of the robot in the map
+	//geometry_msgs::Pose mHeadCurrentPose;		/// Keeps track of the current Pose of the head
+	geometry_msgs::Pose mGoal;					/// Stores the goal postion
+
+	double mDistanceToGoal;						/// Distance to the goal
+	double mDistanceTolerance;					/// Distance tolerance to the goal
+
+	geometry_msgs::PoseStamped mObjectPose;			/// Keeps track of the pose of the found object
+	geometry_msgs::PoseStamped mObjectToArmPose;	/// Keeps track of the pose of the found object in arm space
+
+	Emotions mEmotionalState;					/// Keeps track of Nero's current emotion
+	std::string mSpeech;						/// Keeps track of what is said by the user
+	uint8_t mArousal;							/// Keeps track of the arousal contained in the speech of user
+	bool mWakeUp;								/// Keeps track of whether Nero is listening to commands or not
+	bool mLock;									/// Checks whether the Controller is blocked or not
 
 public:
 	Controller() : mNodeHandle(""){}
@@ -50,7 +80,29 @@ public:
 	virtual ~Controller(){ mNodeHandle.shutdown();}
 
 	void init();
+	void updateRobotPosition();
+	void moveArm(double x, double z);
+	void moveArm(const geometry_msgs::PoseStamped msg);
+	void moveBase(geometry_msgs::PoseStamped &stamped_goal);
+	void moveHead(double x, double z);
+	void findObject(u_int8_t object_id);
+
+	void getJuice();
+
+	void navigationStateCB(const std_msgs::UInt8& msg);
+	void baseGoalCB(const std_msgs::Float32& msg);
 	void speechCB(const audio_processing::speech& msg);
+	//void headPoseCB(const geometry_msgs::Pose& msg);
+	void objectPositionCB(const geometry_msgs::PoseStamped& msg);
+	void waitForLock();
+
+	inline void convertQuaternion(geometry_msgs::Quaternion q1, tf::Quaternion q2)
+	{
+			q1.x = q2.getX();
+			q1.y = q2.getY();
+			q1.z = q2.getZ();
+			q1.w = q2.getW();
+	}
 };
 
 #endif /* CONTROLLER_H_ */
--- logical_unit/src/Controller.cpp
+++ logical_unit/src/Controller.cpp
@@ -7,8 +7,157 @@
 
 #include "logical_unit/Controller.h"
 
-static std::map<std::string, commandValue> stringToValue; /// Maps incoming string to a commandValue for use in switch-case in main function
+/// Maps incoming string to a commandValue for use in switch-case in main function
+static std::map<std::string, commandValue> stringToValue;
 
+/**
+ * Sends a Pose command to AutonomeArmController, given x and z coordinates
+ */
+void Controller::moveArm(double x, double z)
+{
+	geometry_msgs::Pose pose_msg;
+	pose_msg.position.x = x;
+	pose_msg.position.z = z;
+
+	mArmPositionPublisher.publish(pose_msg);
+}
+
+/**
+ * Sends a Pose command to AutonomeArmController, given a Posestamped
+ */
+void Controller::moveArm(const geometry_msgs::PoseStamped msg)
+{
+	mArmPositionPublisher.publish(msg.pose);
+}
+
+void Controller::moveBase(geometry_msgs::PoseStamped &stamped_goal)
+{
+	//TODO: Put this in a function
+	tf::Quaternion goal_orientation(mOriginalPosition.getRotation().getX(), mOriginalPosition.getRotation().getY(), mOriginalPosition.getRotation().getZ(), mOriginalPosition.getRotation().getW());
+
+	//TODO aanpassen om naar tafel te gaan
+	stamped_goal.pose.position.x = std::cos(tf::getYaw(goal_orientation)) + mOriginalPosition.getOrigin().getX();
+	stamped_goal.pose.position.y = std::sin(tf::getYaw(goal_orientation)) + mOriginalPosition.getOrigin().getY();
+
+	convertQuaternion(stamped_goal.pose.orientation, goal_orientation);
+
+	stamped_goal.header.stamp = ros::Time::now();
+
+	mBaseGoalPublisher.publish(stamped_goal);
+}
+/**
+ * Sends a Pose command to AutonomeHeadController
+ */
+void Controller::moveHead(double x, double z)
+{
+	geometry_msgs::Pose pose_msg;
+	pose_msg.position.x = x;
+	pose_msg.position.z = z;
+
+	mHeadPositionPublisher.publish(pose_msg);
+	//wait until the head is at its desired Pose
+	//can be replaced when using direct feedback for the head position
+	usleep(1000000);
+}
+
+/**
+ *	Sends the id of the object to recognize to ObjectRecognition
+ */
+void Controller::findObject(u_int8_t object_id)
+{
+	std_msgs::UInt8 int_msg;
+	int_msg.data = object_id;
+	mObjectRecognitionPublisher.publish(int_msg);
+}
+
+/**
+ *	Listens to the position of the head and updates the head's current pose
+ *
+void Controller::headPoseCB(const geometry_msgs::Pose& msg)
+{
+	mHeadCurrentPose = msg;
+}*/
+
+/**
+ *	Updates robot position on map
+ */
+void Controller::updateRobotPosition()
+{
+	try
+	{
+		mTransformListener.lookupTransform("/map", "/base_link", ros::Time(0), mOriginalPosition);
+	}
+	catch (tf::TransformException ex)
+	{
+		//ROS_ERROR("%s",ex.what());
+	}
+}
+
+/**
+ * Blocks Controller until mLock is free
+ */
+void Controller::waitForLock()
+{
+	while(ros::ok() && mLock)
+	{
+		ros::spinOnce();
+		usleep(200000);
+	}
+}
+
+/*
+ * Method for getting juice
+ */
+void Controller::getJuice()
+{
+	//First, rotate head in the right direction
+
+	//Go to the table
+	updateRobotPosition();
+	/*
+	geometry_msgs::PoseStamped msg;
+	msg.pose.position.x = std::cos(tf::getYaw());
+	msg.pose.position.y = std::sin();
+	 */
+
+	geometry_msgs::PoseStamped msg;
+
+	//moveBase(msg);
+
+	//mLock = true;
+	//waitForLock();
+
+	//ROS_INFO("Reached Goal");
+
+	//Then, lift the arm just above the table
+	moveArm(0.0, 0.0);
+
+	//Aim Kinect to the table
+	moveHead(0.0, 0.0);
+	//TODO measure the correct angles
+
+	//Start object recognition process
+	mLock = true;
+	findObject(9387);
+	waitForLock();
+
+	mTransformListener.transformPose("arm_frame", mObjectPose, mObjectToArmPose);
+
+	//Move arm to object and grab it
+	moveArm(mObjectToArmPose);
+
+	//Tuck in arm while holding object
+	moveArm(MAX_ARM_X_VALUE, MIN_ARM_Z_VALUE);
+
+	//Go back to original position
+	moveBase(msg);
+
+	//Deliver the juice
+}
+
+/**
+ * Listens to user commands and executes them
+ */
 void Controller::speechCB(const audio_processing::speech& msg)
 {
 	mSpeech		 = msg.command;
@@ -21,8 +170,8 @@ void Controller::speechCB(const audio_processing::speech& msg)
 		if(!mWakeUp)
 		{
 			mWakeUp = true;
-			ROS_INFO("Heard user...");
 			//Start initiating actions to show that the robot has heard the user
+			ROS_INFO("Heard user...");
 		}
 		else
 			ROS_INFO("I'm already awake");
@@ -30,8 +179,11 @@ void Controller::speechCB(const audio_processing::speech& msg)
 
 	case JUICE:
 		if(mWakeUp)
+		{
+			//Start initiating actions to get the juice
 			ROS_INFO("Getting juice...");
-		//Start initiating actions to get the juice
+			getJuice();
+		}
 		else
 			ROS_INFO("Don't disturb me in my sleep...");
 		break;
@@ -55,20 +207,88 @@ void Controller::speechCB(const audio_processing::speech& msg)
 }
 //TODO Decide how to score recognized arousal/emotions
 
+/**
+ * Listens to navigation state
+ */
+void Controller::navigationStateCB(const std_msgs::UInt8& msg)
+{
+	if(msg.data == FOLLOW_STATE_FINISHED)
+		mLock = false;
+}
+
+/**
+ * Listens to distances to goal
+ */
+void Controller::baseGoalCB(const std_msgs::Float32& msg)
+{
+	mDistanceToGoal = msg.data;
+}
+
+/**
+ * Listens to poses of the found object
+ */
+void Controller::objectPositionCB(const geometry_msgs::PoseStamped& msg)
+{
+	mObjectPose = msg;
+	mLock = false;
+}
+
+/**
+ * Initialise Controller
+ */
 void Controller::init()
 {
+	//initialise Nero in sleep mode
+	mWakeUp = false;
+
 	//initialise map
 	stringToValue[""]		 = NOTHING;
 	stringToValue["wake up"] = WAKE_UP;
 	stringToValue["juice"]   = JUICE;
 	stringToValue["sleep"]	 = SLEEP;
 
+	mNodeHandle.param<double>("distance_tolerance", mDistanceTolerance, 0.2);
+
 	//initialise subscribers
-	mSpeechSubscriber = mNodeHandle.subscribe("processedSpeechTopic", 1, &Controller::speechCB, this);
+	mSpeechSubscriber 			= mNodeHandle.subscribe("/processedSpeechTopic", 1, &Controller::speechCB, this);
+	mBaseGoalSubscriber 		= mNodeHandle.subscribe("/path_length", 1, &Controller::baseGoalCB, this);
+	mObjectPoseSubscriber		= mNodeHandle.subscribe("/objectPoseFeedbackTopic", 1, &Controller::objectPositionCB, this);
 
 	//initialise publishers
+	mBaseGoalPublisher 			= mNodeHandle.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 1);
+	mArmPositionPublisher 		= mNodeHandle.advertise<geometry_msgs::Pose>("/cmd_arm_position", 1);
+	mHeadPositionPublisher		= mNodeHandle.advertise<geometry_msgs::Pose>("/cmd_head_position", 1);
+	mObjectRecognitionPublisher = mNodeHandle.advertise<std_msgs::UInt8>("cmd_object_recognition", 1);
+
+	//Store goal position
+
+	std::ifstream fin("config/goal.yaml");
+	if (fin.fail())
+	{
+		ROS_ERROR("Failed to open YAML file.");
+		return;
+	}
+
+	YAML::Parser parser(fin);
+	YAML::Node doc;
+	parser.GetNextDocument(doc);
+
+	float yaw;
+	try
+	{
+		doc["goal"][0] >> mGoal.position.x;
+		doc["goal"][1] >> mGoal.position.y;
+		doc["goal"][2] >> yaw;
+
+		convertQuaternion(mGoal.orientation, tf::createQuaternionFromYaw(yaw));
+
+	} catch (YAML::InvalidScalar)
+	{
+		ROS_ERROR("No goal found in yaml file");
+		return;
+	}
+
 	ROS_INFO("Initialised Controller");
-	mWakeUp = false;
 }
 
 int main(int argc, char **argv)
