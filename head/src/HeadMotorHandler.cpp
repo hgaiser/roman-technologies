eca7b965e8082bedb04867b776ebf901bd568eb9
wilson_ko@live.com <wilson_ko@live.com@efe3342a-cd92-a411-41d2-ccb54d0c40cb>
2012-01-17 15:40:05 +0000

    launch file

--- head/src/HeadMotorHandler.cpp
+++ head/src/HeadMotorHandler.cpp
@@ -7,12 +7,16 @@
 
 #include <head/HeadMotorHandler.h>
 
+//TODO: INITIALISE AND STOPHEAD SAFELY
+//TODO: USE QUATERNION PROPERLY
+
 /**
  * Publishes head's current position
  */
 void HeadMotorHandler::publishHeadPosition()
 {
-	mCurrentPose.orientation.x = mDynamixel.getPosition();
+	mCurrentPose.orientation.x = mTilt.getPosition();
+	mCurrentPose.orientation.z = mPan.getPosition();
 	mPositionPub.publish(mCurrentPose);
 }
 
@@ -21,7 +25,24 @@ void HeadMotorHandler::publishHeadPosition()
  */
 void HeadMotorHandler::positionCB(const geometry_msgs::Pose& msg)
 {
-	mDynamixel.setPosition(msg.orientation.x);
+
+	double tilt = msg.orientation.x;
+	double pan 	= msg.orientation.z;
+	//servo.setPosition(msg.orientation.z);
+	if(msg.orientation.x > TILT_UPPER_LIMIT)
+		tilt = TILT_UPPER_LIMIT;
+
+	if(msg.orientation.x < TILT_LOWER_LIMIT)
+		tilt = TILT_LOWER_LIMIT;
+
+	if(msg.orientation.z > PAN_UPPER_LIMIT)
+		pan = PAN_UPPER_LIMIT;
+
+	if(msg.orientation.z < PAN_LOWER_LIMIT)
+		pan = PAN_LOWER_LIMIT;
+
+	mTilt.setPosition(tilt);
+	mPan.setPosition(pan);
 }
 
 /**
@@ -30,12 +51,13 @@ void HeadMotorHandler::positionCB(const geometry_msgs::Pose& msg)
 void HeadMotorHandler::init(char *path)
 {
 	//Initialise publishers
-	mPositionPub = mNodeHandle.advertise<geometry_msgs::Pose>("/headPositionFeedbackTopic", 1);
+	mPositionPub	= mNodeHandle.advertise<geometry_msgs::Pose>("/headPositionFeedbackTopic", 1);
 
 	//Initialise subscribers
-	mPositionSub 	= mNodeHandle.subscribe("/headPositionTopic", 10, &HeadMotorHandler::positionCB, this);
+	mPositionSub	= mNodeHandle.subscribe("/headPositionTopic", 10, &HeadMotorHandler::positionCB, this);
 
-	mDynamixel.init(path);
+	mTilt.init(path);
+	mPan.init(path);
 
 	ROS_INFO("Initialising completed.");
 }
