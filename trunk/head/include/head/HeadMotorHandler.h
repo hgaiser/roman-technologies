7d4f0068fc6e5c15e79cc233944c7451a9bf72af
wilson_ko@live.com <wilson_ko@live.com@efe3342a-cd92-a411-41d2-ccb54d0c40cb>
2012-01-17 15:40:05 +0000

    launch file

--- trunk/head/include/head/HeadMotorHandler.h
+++ trunk/head/include/head/HeadMotorHandler.h
@@ -12,6 +12,12 @@
 #include <geometry_msgs/Pose.h>
 #include <motors/DynamixelMotor.h>
 
+#define TILT_UPPER_LIMIT	1.25
+#define TILT_LOWER_LIMIT	(-1)
+
+#define PAN_UPPER_LIMIT		1.25
+#define PAN_LOWER_LIMIT		(-1)
+
 class HeadMotorHandler
 {
 
@@ -22,13 +28,14 @@ protected:
 	ros::Publisher mPositionPub;			/// Publishes current head position
 	geometry_msgs::Pose mCurrentPose;		/// Keeps track of the current pose of the head
 
-	DynamixelMotor mDynamixel;				/// Dynamixel motor
+	DynamixelMotor mTilt;					/// Dynamixel motor for tilting the head
+	DynamixelMotor mPan;					/// Dynamixel motor for panning the head
 
 	MotorId mMotorId;						/// Id of the motor that is currently being controlled
 
 public:
 	/// Constructor
-	HeadMotorHandler() : mNodeHandle("~"), mDynamixel(DYNAMIXEL_ID) { }
+	HeadMotorHandler() : mNodeHandle("~"), mTilt(TILT_ID), mPan(PAN_ID) { }
 
 	/// Destructor
 	/** Delete motor interface, close serial port, and shut down node handle */
