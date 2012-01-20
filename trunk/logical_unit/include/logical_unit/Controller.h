56db817d4920c1a521aa67642190365a791c1ec0
wilson_ko@live.com <wilson_ko@live.com@efe3342a-cd92-a411-41d2-ccb54d0c40cb>
2012-01-20 13:15:48 +0000

    logical unit Controller commit

--- trunk/logical_unit/include/logical_unit/Controller.h
+++ trunk/logical_unit/include/logical_unit/Controller.h
@@ -13,7 +13,6 @@
 #include <std_msgs/Float32.h>
 #include <logical_unit/PathFollower.h>
 #include <audio_processing/speech.h>
-#include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Pose.h>
@@ -21,10 +20,12 @@
 #include "fstream"
 #include <map>
 
-#define MIN_ARM_Z_VALUE -32.1
-#define MAX_ARM_Z_VALUE 17.25
-#define MIN_ARM_X_VALUE -20
-#define MAX_ARM_X_VALUE 35.95
+#define MIN_ARM_Z_VALUE (-0.321)
+#define MAX_ARM_Z_VALUE (0.1725)
+#define MAX_ARM_X_VALUE (0.20)
+#define MIN_ARM_X_VALUE (-0.3595)
+
+#define VIEW_OBJECTS_ANGLE	0.4
 
 enum commandValue
 {
@@ -42,37 +43,47 @@ enum Emotions
 			SURPRISED,
 };
 
+enum Locks
+{
+	NONE = 0,
+			BASE_LOCK,
+			HEAD_LOCK,
+			ARM_LOCK,
+			GRIPPER_LOCK,
+			OBJECT_RECOGNITION_LOCK,
+};
+
 class Controller
 {
 private:
 	ros::NodeHandle mNodeHandle;
 
-	ros::Subscriber mSpeechSubscriber;			/// Listens to speech processed by SpeechRecognition node
-	ros::Subscriber mObjectPoseSubscriber;		/// Listens to the Pose of the found object
-	ros::Subscriber mBaseGoalSubscriber;		/// Listens to path distances to see whether goal is reached
-	ros::Subscriber mNavigationStateSubscriber;	///	Listens to the state of the navigation algorithm
+	ros::Subscriber mSpeechSubscriber;				/// Listens to speech processed by SpeechRecognition node
+	ros::Subscriber mObjectPoseSubscriber;			/// Listens to the Pose of the found object
+	ros::Subscriber mBaseGoalSubscriber;			/// Listens to path distances to see whether goal is reached
+	ros::Subscriber mNavigationStateSubscriber;		///	Listens to the state of the navigation algorithm
 
-	ros::Publisher mBaseGoalPublisher;			/// Publishes goal commands to PathPlanner
-	ros::Publisher mArmPositionPublisher;		/// Publishes coordinates to AutonomeArmController
-	ros::Publisher mHeadPositionPublisher;		/// Publishes coordinates to AutonomeHeadController
-	ros::Publisher mObjectRecognitionPublisher;	/// Publishes ID of the object to recognize
+	ros::Publisher mBaseGoalPublisher;				/// Publishes goal commands to PathPlanner
+	ros::Publisher mArmPositionPublisher;			/// Publishes coordinates to AutonomeArmController
+	ros::Publisher mHeadPositionPublisher;			/// Publishes coordinates to AutonomeHeadController
+	ros::Publisher mObjectRecognitionPublisher;		/// Publishes ID of the object to recognize
 
-	tf::TransformListener mTransformListener;	/// Fills mOriginalPosition
-	tf::StampedTransform mOriginalPosition;		/// Keeps track of the original position of the robot in the map
-	//geometry_msgs::Pose mHeadCurrentPose;		/// Keeps track of the current Pose of the head
-	geometry_msgs::Pose mGoal;					/// Stores the goal postion
+	tf::TransformListener mTransformListener;		/// Fills mOriginalPosition
+	tf::StampedTransform mOriginalPosition;			/// Keeps track of the original position of the robot in the map
+	geometry_msgs::Pose mHeadCurrentPose;			/// Keeps track of the current Pose of the head
+	geometry_msgs::Pose mGoal;						/// Stores the goal postion
 
-	double mDistanceToGoal;						/// Distance to the goal
-	double mDistanceTolerance;					/// Distance tolerance to the goal
+	double mDistanceToGoal;							/// Distance to the goal
+	double mDistanceTolerance;						/// Distance tolerance to the goal
 
 	geometry_msgs::PoseStamped mObjectPose;			/// Keeps track of the pose of the found object
 	geometry_msgs::PoseStamped mObjectToArmPose;	/// Keeps track of the pose of the found object in arm space
 
-	Emotions mEmotionalState;					/// Keeps track of Nero's current emotion
-	std::string mSpeech;						/// Keeps track of what is said by the user
-	uint8_t mArousal;							/// Keeps track of the arousal contained in the speech of user
-	bool mWakeUp;								/// Keeps track of whether Nero is listening to commands or not
-	bool mLock;									/// Checks whether the Controller is blocked or not
+	Emotions mEmotionalState;						/// Keeps track of Nero's current emotion
+	std::string mSpeech;							/// Keeps track of what is said by the user
+	uint8_t mArousal;								/// Keeps track of the arousal contained in the speech of user
+	bool mWakeUp;									/// Keeps track of whether Nero is listening to commands or not
+	Locks mLock;									/// Checks whether the Controller is blocked or not and keeps track of which node it is being blocked by
 
 public:
 	Controller() : mNodeHandle(""){}
@@ -94,14 +105,15 @@ public:
 	void speechCB(const audio_processing::speech& msg);
 	void headPoseCB(const geometry_msgs::Pose& msg);
 	void objectPositionCB(const geometry_msgs::PoseStamped& msg);
+
 	void waitForLock();
 
 	inline void convertQuaternion(geometry_msgs::Quaternion q1, tf::Quaternion q2)
 	{
-			q1.x = q2.getX();
-			q1.y = q2.getY();
-			q1.z = q2.getZ();
-			q1.w = q2.getW();
+		q1.x = q2.getX();
+		q1.y = q2.getY();
+		q1.z = q2.getZ();
+		q1.w = q2.getW();
 	}
 };
 
