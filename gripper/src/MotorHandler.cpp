6915a43a1fe3cfa9cab85d0dfada448ead2701dc
wilson_ko@live.com <wilson_ko@live.com@efe3342a-cd92-a411-41d2-ccb54d0c40cb>
2012-01-27 12:51:32 +0000

    New gripper MotorHandler, making use of Motor.cpp

--- gripper/src/MotorHandler.cpp
+++ gripper/src/MotorHandler.cpp
@@ -8,20 +8,20 @@
 /**
  * Called when a motor command is received over the motor topic.
  */
-void MotorHandler::chatterCallback(const gripper::MotorControlPtr& mc)
+void MotorHandler::gripperCB(const gripper::MotorControl& mc)
 {
     // print out message for debugging
     std::stringstream ss("");
-    ss << mc->modeStr << " " << mc->value << " " << mc->waitTime;
+    ss << mc.modeStr << " " << mc.value << " " << mc.waitTime;
     ROS_INFO("Received message: %s", ss.str().c_str());
 
     // parse the control mode
     ControlMode mode = CM_NONE;
-    if (strcmp(mc->modeStr.c_str(), "torque") == 0)
+    if (strcmp(mc.modeStr.c_str(), "torque") == 0)
         mode = CM_TORQUE_MODE;
-    else if (strcmp(mc->modeStr.c_str(), "current") == 0)
+    else if (strcmp(mc.modeStr.c_str(), "current") == 0)
         mode = CM_CURRENT_MODE;
-    else if (strcmp(mc->modeStr.c_str(), "stop") == 0)
+    else if (strcmp(mc.modeStr.c_str(), "stop") == 0)
         mode = CM_STOP_MODE;
 
     // some wrong message?
@@ -31,28 +31,30 @@ void MotorHandler::chatterCallback(const gripper::MotorControlPtr& mc)
     // Are we changing modes? Then let the motor know.
     if (mode != cmode)
     {
-        motor_->set3MxlMode(mode);
+        mGripperMotor.setMode(cmode);
         cmode = mode;
     }
     // pass the value to the motor
     switch (mode)
     {
     case CM_TORQUE_MODE:
-        motor_->setTorque(mc->value);
+    		mGripperMotor.setTorque(mc.value);
         break;
+
     case CM_CURRENT_MODE:
-        motor_->setCurrent(mc->value);
+        mGripperMotor.setCurrent(mc.value);
         break;
+
     default:
         break;
     }
 
-    // do we need to sleep for a bit and then disable the motor ? (Use threads?)
-    if (mc->waitTime)
+    // Do we need to sleep for a bit and then disable the motor ? (Use threads?)
+    if (mc.waitTime)
     {
-        usleep(mc->waitTime * 1000);
-        motor_->set3MxlMode(CM_STOP_MODE);
-        motor_->set3MxlMode(cmode);
+        usleep(mc.waitTime * 1000);
+        mGripperMotor.setMode(CM_STOP_MODE);
+        mGripperMotor.setMode(cmode);
     }
 }
 
@@ -61,38 +63,16 @@ void MotorHandler::chatterCallback(const gripper::MotorControlPtr& mc)
 */
 void MotorHandler::init(char *path)
 {
-    CDxlConfig *config = new CDxlConfig();
+	//Initialise subscribers
+	mMotorSub = mNodeHandle.subscribe("/gripperMotorTopic", 1, &MotorHandler::gripperCB, this);
 
-    // find the connection to the motor on the 3mXl board
-    if (path)
-    {
-        ROS_INFO("Using shared_serial");
-        motor_ = new C3mxlROS(path);
-    }
-    else
-    {
-        ROS_INFO("Using direct connection");
-        motor_ = new C3mxl();
-    
-        serial_port_.port_open("/dev/roman/threemxl", LxSerial::RS485_FTDI);
-        serial_port_.set_speed(LxSerial::S921600);
-        motor_->setSerialPort(&serial_port_);
-    }
-
-    // initialize the motor
-    motor_->setConfig(config->setID(109));
-    motor_->init(false);
-    motor_->set3MxlMode(TORQUE_MODE);
-
-    delete config;
-    ROS_INFO("Initializing completed."); 
+	mGripperMotor.init(path);
+	ROS_INFO("Initialising completed.");
 }
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "listener");
-
-    ros::NodeHandle n;
+    ros::init(argc, argv, "gripperMotorHandler");
 
     char *path=NULL;
     if (argc == 2)
@@ -101,11 +81,7 @@ int main(int argc, char **argv)
     MotorHandler motorHandler;
     motorHandler.init(path);
 
-    ros::Subscriber sub = n.subscribe("motorTopic", 1, &MotorHandler::chatterCallback, &motorHandler);
-
     ros::spin();
-
-
     return 0;
 }
 
