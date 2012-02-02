ba14bd78011f4e09ba6ce27f68fa8f5e6101e09c
hansg91@gmail.com <hansg91@gmail.com@efe3342a-cd92-a411-41d2-ccb54d0c40cb>
2012-02-02 17:31:55 +0000

    Added error state after 6 bumper hits

--- trunk/mobile_base/include/mobile_base/SafeKeeper.h
+++ trunk/mobile_base/include/mobile_base/SafeKeeper.h
@@ -13,7 +13,10 @@
 #include <mobile_base/SensorFeedback.h>
 #include <mobile_base/position.h>
 #include <std_msgs/UInt8.h>
+#include <head/Emotion.h>
 
+#define ERROR_STATE_BUMPER_COUNT 6
+#define ERROR_STATE_BUMPER_TIME 60.0
 
 #define SAFE_DISTANCE  0.5
 
@@ -37,6 +40,7 @@ private:
 	ros::Subscriber mSpeed_sub;					/// Subscriber to current speed of mobile base
 
 	ros::Publisher  mMovement_pub;				/// Publisher to control the motors
+	ros::Publisher	mEmotion_pub;				///
 	BumperState 	mBumperState;				/// Keeps track of the current state for the bumpers
 	bool mDisableForward, mDisableBackward;		/// Keeps track of the front and backward buttons of the bumpers
 
@@ -44,8 +48,11 @@ private:
 
 	boost::array<int16_t, 10> mSensorData;
 
+	uint32_t mBumperCount;
+	double mBumperStartTime;
+
 public:
-	SafeKeeper() :  mBumperState(BUMPER_NONE) {};
+	SafeKeeper() :  mBumperState(BUMPER_NONE), mBumperCount(0), mBumperStartTime(0.0) {};
 
 	void init();
 	void bumperFeedbackCB(const std_msgs::UInt8 &msg);
