f772a0b5ada6f95e5269abc2d7867344313a9fc1
hansg91@gmail.com <hansg91@gmail.com@efe3342a-cd92-a411-41d2-ccb54d0c40cb>
2012-07-05 09:07:10 +0000

    Modified FocusFace to use the Head/follow_point topic and limited its range (NTY) logical_unit/Controller is now awake by default

--- trunk/image_processing/include/image_processing/FocusFace.h
+++ trunk/image_processing/include/image_processing/FocusFace.h
@@ -42,6 +42,7 @@ class FocusFace
 private:
 	ros::NodeHandle mNodeHandle;
 	image_transport::ImageTransport mImageTransport;
+	tf::TransformListener mTransformListener;
 
 	cv::CascadeClassifier mFrontalFaceCascade;
 	cv::CascadeClassifier mProfileCascade;
@@ -61,13 +62,15 @@ private:
 	ros::ServiceClient mQueryCloudClient;
 	ros::ServiceClient mForceDepthClient;
 
-	nero_msgs::PitchYaw mCurrentOrientation;
 	nero_msgs::PitchYaw mCurrentSpeed;
 	bool mActive;
 	double mScale;
 	bool mDisplayFrames;
 	bool mDetectOnly;
 	bool mSendHeadPosition;
+	double mMinHeight;
+	double mMaxHeight;
+	double mMaxDistance;
 
 	inline bool canMoveHead() { return mCurrentSpeed.pitch < STOP_SPEED_TOLERANCE && mCurrentSpeed.yaw < STOP_SPEED_TOLERANCE; };
 
